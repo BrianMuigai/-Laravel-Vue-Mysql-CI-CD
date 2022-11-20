f486021dc2e328085bcd775252ffe31b4a8160af
Brian <brian@hava.bz>
2022-11-20 09:37:54 +0300

    test cases

---
+++ tests/Feature/StoreItemWithoutNameTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class StoreItemWithoutName extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $data = [
+            'item' => []
+        ];
+        $response = $this->post('api/item/store', $data);
+        $response->assertStatus(200); //should return 500
+    }
+}
